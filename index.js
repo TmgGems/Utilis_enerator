5c6f05a7cd01f1edd546632283cf0d2e59e5faf2
TmgGems <gyalwanglama123@gmail.com>
2023-06-25 13:50:04 +0545

    Learned Express Server

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/",(req,res) =>{
+    res.send("Hello World");
+})
+
 app.listen(8000, () => {// method vitra method call back function
     console.log("Server running on port 8000");
 })
