b65b5f34b5b947a01dd3a6c3c95faa375647fbb6
TmgGems <gyalwanglama123@gmail.com>
2023-06-25 14:36:13 +0545

    API routes but

---
+++ router/routes.ui.js
@@ -0,0 +1,8 @@
+//UI location manage garxa
+const express = require("express");
+const router = express.Router();
+
+router.get("/",(req,res) =>{
+    res.send("Hello From UI");
+})
+module.exports = router;
