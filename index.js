e3c2d30ab88d071b0df59b1b83348dd3e9cd0de6
TmgGems <gyalwanglama123@gmail.com>
2023-06-27 14:06:43 +0545

    Middle ware

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 
 const app = express();
-
+const morgan =require("morgan");
 const indexRouter = require("./router");
 
-app.use("/",indexRouter)
+
+
+
+//middleware
+// app.use((req,res,next) =>{
+//     console.log("Request received at" + Date.now());
+// });
+app.use(morgan("tiny"));
+app.use("/",indexRouter);
+
 
 app.listen(8000, () => {// method vitra method call back function
     console.log("Server running on port 8000");
